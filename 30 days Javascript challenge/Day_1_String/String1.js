d8d7e8b19fef1aae4ad7201b8b91e26c63d42b28
chinmaya-kumar-das <chinmayakumardas2000@gmail.com>
2024-02-14 18:11:03 +0530

    dcdw

---
+++ 30 days Javascript challenge/Day_1_String/String1.js
@@ -0,0 +1,36 @@
+let names="    chinmaya  kumar   das ";
+let age=22;
+let str="1"
+
+console.log(names.length);
+console.log(names.indexOf("k"));
+console.log(names.charAt(5));
+console.log(names.includes("k u"));
+console.log(names.charCodeAt(10));
+console.log(names.trim());
+console.log(names.trimStart());
+console.log(names.trimEnd());
+console.log(names.replace("kumar","kumara"));
+console.log(names.replaceAll("a","b"));
+
+console.log(names.lastIndexOf("a"));
+console.log(names.substring(1,10));
+console.log(names.substr(6,5));
+console.log(names.split(" "));
+console.log(names.search("chinmaya"));
+console.log(names.slice(5,10));
+console.log(names.concat(" is very good boy"));
+console.log(names.concat(age));
+console.log((5,"0"));
+console.log();
+
+console.log(names.toLocaleLowerCase());
+console.log(names.toUpperCase());
+console.log(str.padEnd(5,"0"));
+console.log(str.padStart(5,"0"));
+console.log(str.repeat(10));
+console.log(str.at(2));
+console.log(names.match("a"));
+console.log(names.matchAll("a"));
+console.log(`${str} ${names}`);
+console.log();
